25a34cacb3f59b463dc33c0bdcad98503d98c7e2
ByeongUk Choi <cbw0916@gmail.com>
2022-04-14 23:56:13 +0900

    feat add Stove.turn_on/1

--- lib/restaurant/kitchen/stove.ex
+++ lib/restaurant/kitchen/stove.ex
@@ -6,13 +6,26 @@ defmodule Restaurant.Kitchen.Stove do
   """
   use GenServer
 
+  alias Restaurant.Kitchen.Burner
+
   # API
   def start_link(burner_count) do
     GenServer.start_link(__MODULE__, burner_count, name: __MODULE__)
   end
 
+  def turn_on(time) do
+    GenServer.cast(__MODULE__, {:turn_on, time})
+  end
+
   # Server
   def init(burner_count) do
-    {:ok, count: burner_count, burners: MapSet.new()}
+    {:ok, %{max_count: burner_count, burners: MapSet.new()}}
+  end
+
+  def handle_cast({:turn_on}, %{max_count: max_count, burners: burners}) do
+    if MapSet.size(burners) < max_count do
+      {:ok, pid} = Burner.start_link(nil)
+      {:noreply, %{max_count: max_count, burners: MapSet.put(burners, pid)}}
+    end
   end
 end
