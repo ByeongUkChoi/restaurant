fe2f429b2d0b6c23517ef2239b82406de8fd2fe3
ByeongUk Choi <cbw0916@gmail.com>
2022-05-04 00:23:56 +0900

    feat. add order_queue api

--- lib/restaurant/order_queue.ex
+++ lib/restaurant/order_queue.ex
@@ -13,6 +13,14 @@ defmodule Restaurant.OrderQueue do
     GenServer.call(__MODULE__, {:enqueue, item})
   end
 
+  def denqueue() do
+    GenServer.cast(__MODULE__, :denqueue)
+  end
+
+  def list() do
+    GenServer.cast(__MODULE__, :list)
+  end
+
   # Server
   def init(_) do
     {:ok, :queue.new()}
@@ -20,5 +28,14 @@ defmodule Restaurant.OrderQueue do
 
   def handle_cast({:enqueue, item}, queue) do
     {:noreply, :queue.in(item, queue)}
+  end
+
+  def handle_call(:denqueue, _from, queue) do
+    {{:value, item}, queue} = :queue.out(queue)
+    {:reply, item, queue}
+  end
+
+  def handle_call(:list, _from, queue) do
+    {:reply, :queue.to_list(queue), queue}
   end
 end
