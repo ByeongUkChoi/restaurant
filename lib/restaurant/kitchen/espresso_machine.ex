c18924ccc922cf74adf4e2749c1eed5cc0f98c41
ByeongUk Choi <cbw0916@gmail.com>
2022-05-08 11:57:28 +0900

    add espresso machine

---
+++ lib/restaurant/kitchen/espresso_machine.ex
@@ -0,0 +1,43 @@
+defmodule Restaurant.Kitchen.EspressoMachine do
+  @moduledoc """
+  extract esspreso
+  """
+  use GenServer
+
+  # API
+  def start_link(_group_count) do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def extract() do
+    GenServer.cast(__MODULE__, :extract)
+  end
+
+  # Server
+  def init(_) do
+    {:ok, nil}
+  end
+
+  def handle_cast(:extract, nil) do
+    Process.send_after(self(), :timer, 1000)
+    {:noreply, 30}
+  end
+
+  def handle_cast(:extract, remaining_time) do
+    {:noreply, remaining_time}
+  end
+
+  def handle_info(:timer, nil) do
+    {:noreply, nil}
+  end
+
+  def handle_info(:timer, remaining_time) when remaining_time <= 0 do
+    {:noreply, remaining_time}
+  end
+
+  def handle_info(:timer, remaining_time) do
+    Process.send_after(self(), :timer, 1000)
+    update_time = if remaining_time - 1 > 0, do: remaining_time - 1, else: 0
+    {:noreply, update_time}
+  end
+end
