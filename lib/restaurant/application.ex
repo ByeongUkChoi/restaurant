434bd8d6bdbe3da123d4746b1bbed40fdca3331f
ByeongUk Choi <cbw0916@gmail.com>
2022-05-11 00:29:35 +0900

    feat. add espresso machine multi groups

--- lib/restaurant/application.ex
+++ lib/restaurant/application.ex
@@ -19,7 +19,7 @@ defmodule Restaurant.Application do
       # Start a worker by calling: Restaurant.Worker.start_link(arg)
       # {Restaurant.Worker, arg}
       {Restaurant.Kitchen.Stove, 4},
-      Restaurant.Kitchen.EspressoMachine,
+      {Restaurant.Kitchen.EspressoMachine, 3},
       Restaurant.OrderQueue
     ]
 
--- lib/restaurant/kitchen/espresso_machine.ex
+++ lib/restaurant/kitchen/espresso_machine.ex
@@ -4,9 +4,14 @@ defmodule Restaurant.Kitchen.EspressoMachine do
   """
   use GenServer
 
+  @type state :: %{
+          group_count: non_neg_integer(),
+          groups: list(%{required(:id) => integer(), required(:time) => non_neg_integer()})
+        }
+
   # API
-  def start_link(_group_count) do
-    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  def start_link(group_count) do
+    GenServer.start_link(__MODULE__, group_count, name: __MODULE__)
   end
 
   def extract() do
@@ -18,8 +23,9 @@ defmodule Restaurant.Kitchen.EspressoMachine do
   end
 
   # Server
-  def init(_) do
-    {:ok, nil}
+  def init(group_count) do
+    state = %{group_count: group_count, groups: Enum.map(1..group_count, &%{id: &1, time: 0})}
+    {:ok, state}
   end
 
   def handle_cast(:extract, nil) do
@@ -35,17 +41,15 @@ defmodule Restaurant.Kitchen.EspressoMachine do
     {:reply, remaining_time, remaining_time}
   end
 
-  def handle_info(:timer, nil) do
-    {:noreply, nil}
-  end
+  def handle_info({:timer, id}, state) do
+    remaining_time = Enum.find_value(state, &(&1.id == id && &1.time))
 
-  def handle_info(:timer, remaining_time) when remaining_time <= 0 do
-    {:noreply, remaining_time}
-  end
-
-  def handle_info(:timer, remaining_time) do
-    Process.send_after(self(), :timer, 1000)
-    update_time = if remaining_time - 1 > 0, do: remaining_time - 1, else: 0
-    {:noreply, update_time}
+    if remaining_time > 0 do
+      Process.send_after(self(), {:timer, id}, 1000)
+      update_time = if remaining_time - 1 > 0, do: remaining_time - 1, else: 0
+      {:noreply, update_time}
+    else
+      {:noreply, remaining_time}
+    end
   end
 end
